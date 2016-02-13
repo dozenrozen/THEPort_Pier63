a386a1914906727e5cf8e7bcb1d7d4db250fba95
blumen <johan.blumenberg@gmail.com>
2016-02-13 13:50:19 +0100

    Initial app

---
+++ camper/www/js/app.js
@@ -0,0 +1,66 @@
+var app = angular.module('bridgeItApp', ['ionic', 'ngCordova'])
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if(window.StatusBar) StatusBar.styleDefault();
+    });
+  })
+  .config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    $httpProvider.defaults.withCredentials = true;
+
+    delete $httpProvider.defaults.headers.common["X-Requested-With"];
+
+    $httpProvider.defaults.headers.common["Accept"] = "application/json";
+    $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
+  }])
+  .config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+    // Routes (found in templates folder)
+    $stateProvider
+      .state('main', {
+        cache: false,
+        url: "/",
+        templateUrl: "templates/main.html",
+        controller: "mainCtrl"
+      })
+      .state('health', {
+        cache: false,
+        url: "/next",
+        templateUrl: "templates/health.hbs",
+        controller: "healthCtrl"
+      });
+    $urlRouterProvider.otherwise('/');
+
+    // Removes transitions (else looks ugly with background image)
+    $ionicConfigProvider.views.transition('none');
+  });
+
+
+// Controllers
+app
+  .controller('mainCtrl', function($scope, $cordovaMedia) {
+    $scope.playAudio = function() {
+      var src = "/android_asset/www/audio/audio_example.mp3";
+      var media = $cordovaMedia.newMedia(src);
+		media.setVolume(1);
+      media.play();
+    }
+  })
+  .controller('healthCtrl', function($scope, $http) {
+    $scope.title = 'Health';
+    //$http.get('http://makoto2014.com/port15/get_categories.php')
+    //  .success(function(data) {
+    //    $scope.posts = data;
+    //    console.log(data);
+    //  })
+    //  .error(function(data, status, headers, config) {
+    //    console.log(status);
+    //  });
+
+
+    // Let's mock it up for now
+  });
